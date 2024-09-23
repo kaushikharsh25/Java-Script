8d0aa951a3b096cd76d55407c85de95663a9cdd3
kaushikharsh25 <145558406+kaushikharsh25@users.noreply.github.com>
2024-09-23 14:04:59 +0000

    commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 1243
+let emailId = "harsh@gamil.com"
+var accountPass = "12354"
+accountCity = "jaipur"
+
+let accountState;
+// this can also work if we dont define or intitialised the value in JS
+// it gives an value undefined
+
+
+console.log(accountId);
+
+console.table([accountId,accountPass,emailId,accountCity,accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,14 @@
+"use strict" // treat all js code as new version
+
+// alert(8+2)  // we are using Nodejs , not browser
+
+let name = "Harsh"
+let age = 12
+let isLogged = true
+let state = null
+
+
+// number , String , boolean , null => Stand alone value , undefined , symbol-> used for uniqueness , object 
+
+// console.log(typeof null);      // it like an error in language  // Object 
+// console.log(typeof undefined);
