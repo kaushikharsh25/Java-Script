d8cfe96d3483eb325460fbf9c200d58f8b54104e
kaushikharsh25 <145558406+kaushikharsh25@users.noreply.github.com>
2024-10-03 13:26:20 +0000

    arrow part 2

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -10,9 +10,9 @@ const user = {
 
 }
 
-user.welcomeMessage()
-user.name = "sam"
-user.welcomeMessage()
+// user.welcomeMessage()
+// user.name = "sam"
+// user.welcomeMessage()
 
 // console.log(this);   // current context is empty because it is in node enviornment 
 
@@ -23,4 +23,29 @@ user.welcomeMessage()
 
 // chai()
 
-const chai = function
+// const chai = () => {
+//     let username = "potter"
+//         console.log(this.username); 
+// }
+// chai();
+
+// explicit return 
+
+
+// const addTwo = (num1,num2) =>{
+//     // return num1+num2
+//     console.log(`the sum of ${num1} and ${num2} is ${num1+num2}`);
+// }
+
+// implicit return 
+
+// const addTwo = (num1,num2) => (num1+num2)
+
+
+const addTwo = (num1,num2) => ({username:"harsh"})      // to return object also we have to cover it with paranthesis , if we have to write it in implicitly then we have to cover it in parenthesis 
+
+
+console.log(addTwo(2,3));
+// addTwo(2,3)
+// console.log(typeof addTwo);   // now addTwo is the function type
+
